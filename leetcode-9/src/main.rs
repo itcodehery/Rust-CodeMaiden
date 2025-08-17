273029bc9894dfa1643eed7ff434447b05752d7e
Hari Prasad <haririo321@gmail.com>
2025-08-17 16:53:24 +0530

    (leetcode-9) added implementation for Palindrome
    One-liner form in Rust using String

---
+++ leetcode-9/src/main.rs
@@ -0,0 +1,16 @@
+#[macro_use]
+extern crate text_io;
+
+pub fn is_palindrome(x: i32) -> bool {
+    return x.to_string().chars().rev().eq(x.to_string().chars());
+}
+
+fn main() {
+    println!("Enter a number: ");
+    let input: String = read!();
+    let input = input.trim();
+    match input.parse::<i32>() {
+        Ok(j) => println!("Is {} a Palindrome?:  {}", j, is_palindrome(j)),
+        Err(..) => println!("Input is not an integer: {}", input),
+    }
+}
